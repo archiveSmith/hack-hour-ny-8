25c16424abd8db14d1f0ed4ca985470de244e529
Taihyun Lee <th9061@hotmail.com>
2018-11-14 09:35:55 -0500

    completed hack hour challenge

--- challenges/fizzbuzz.js
+++ challenges/fizzbuzz.js
@@ -1,4 +1,5 @@
-// Write a function that returns an array containing the numbers 1 to NUM. Put "fizz" in place of numbers divisble by 3, "buzz" in place of numbers divisble by 5, and "fizzbuzz" in place of numbers divisble by both 3 and 5
+// Write a function that returns an array containing the numbers 1 to NUM. Put "fizz" in place of numbers divisble by 3, 
+// "buzz" in place of numbers divisble by 5, and "fizzbuzz" in place of numbers divisble by both 3 and 5
 // fizzbuzz(16);  -> [ 1,
 //                     2,
 //                     'fizz',
@@ -17,7 +18,21 @@
 //                     16 ]
 
 function fizzbuzz(num) {
-
+    let result = [];
+    for(var i=1; i<=num; i++) {
+        if(i%3 === 0 && i%5 === 0) {
+            result.push("fizzbuzz");
+        } else if(i%3 === 0) {
+            result.push("fizz");
+        } else if(i%5 === 0) {
+            result.push("buzz");
+        } else {
+            result.push(i);
+        }
+    }
+    return result;
 }
+
+console.log(fizzbuzz(16));
 
 module.exports = fizzbuzz;
