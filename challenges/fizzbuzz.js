567adcff838895feb4d4490de1664fbed19958d3
Woojae Park <wjp241@nyu.edu>
2018-11-14 09:21:45 -0500

    jae park: hackhours fizzbuzz complete

--- challenges/fizzbuzz.js
+++ challenges/fizzbuzz.js
@@ -17,7 +17,21 @@
 //                     16 ]
 
 function fizzbuzz(num) {
-
+  const arr = [];
+  for(let i = 1; i <= num; i ++) {
+    if (i % 3 === 0) {
+      arr.push('fizz');
+      } else if (i % 5 === 0) {
+        arr.push('buzz');
+      } else if (i % 3 === 0 && i % 5 === 0 ) {
+        arr.push("fizzbuzz")
+      } else {
+        arr.push(i);
+      }
+  }
+  return arr;
 }
+
+fizzbuzz(16);
 
 module.exports = fizzbuzz;
