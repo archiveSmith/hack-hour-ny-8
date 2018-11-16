15c8c27fb9fb2be1dceea71e66ffb1e4bb60268f
Taihyun Lee <th9061@hotmail.com>
2018-11-16 09:58:52 -0500

    completed the draw-stair.js

---
+++ challenges/draw-stairs.js
@@ -0,0 +1,36 @@
+/* Write a function that console logs a staircase of any given height where 1 <= N <= 100.
+ The staircase must climb up from left to right. The last line should only consist of asterisks,
+ without any leading/trailing spaces.
+ 
+ For example:     
+ drawStairs(6) ->          
+     *
+    **
+   ***
+  ****
+ *****
+******
+
+*/
+
+function drawStairs(n) {
+    if(n > 100 || n < 1) {
+        throw new Exception ("The number is either too big or too small");
+    }
+
+    for(let i=0; i<n; i++) {
+        let j = 0;
+        let pr = "";
+        for(; j<n-i-1; j++) {
+            pr += " ";
+        }
+        for(let k=j; k<n; k++) {
+            pr += "*";
+        }
+        console.log(pr);
+    }
+}
+
+
+
+module.exports = drawStairs;
