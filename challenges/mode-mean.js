2d507c1a92c3e7c6a2118fe523329dbc6a8e6fb6
GeneTheBean <benesitis@gmail.com>
2018-11-17 09:49:32 -0500

    3

--- challenges/mode-mean.js
+++ challenges/mode-mean.js
@@ -9,9 +9,27 @@
  *
  */
 
-
 function modemean(array) {
-
+  const map = new Map();
+  let total = 0;
+  for (item of array) {
+    total += item;
+    count = map.get(item) || 0;
+    map.set(item, count + 1);
+  }
+  const mean = Math.floor(total / array.length);
+  let max = Number.NEGATIVE_INFINITY;
+  let modes = [];
+  for (let [key, value] of map) {
+    if (value === max) {
+      modes.push(key);
+    } else if (value > max) {
+      max = value;
+      mode = [];
+      modes.push(key);
+    }
+  }
+  return mean === modes.sort()[modes.length - 1];
 }
 
 module.exports = modemean;
