6be914753616e3cd6aa4c72c7ea427de2dc3f17c
bbbryan14 <bryan.li@foxmail.com>
2018-11-21 09:53:53 -0500

    pow

--- challenges/pow.js
+++ challenges/pow.js
@@ -3,7 +3,16 @@
  */
 
 function pow(base, power) {
-
+    if (base === 0 && power === 0) return 1;
+    if (power === 0) return 1;
+    if (power === 1) return base;
+    if (power > 0) {
+        return base * pow(base, power - 1);
+    }
+    if (power < 0) {
+        power = power * -1;
+        return 1 / (base * pow(base, power - 1));
+    }
 }
 
 module.exports = pow;
