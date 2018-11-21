4fac672a4f624310591349cd00f82453dc454ba8
Woojae Park <wjp241@nyu.edu>
2018-11-21 09:54:31 -0500

    both positive and negative

--- challenges/pow.js
+++ challenges/pow.js
@@ -4,9 +4,15 @@
 
 function pow(base, power) {
   if(power === 0) return 0;
-  if(power === 1) return base;
 
-  return base * pow(base, power - 1);
+  if(power > 0) {
+    if(power === 1) return base;
+    return base * pow(base, power - 1);
+  } else {
+    if(power === -1) {return (1/base);}
+    return (1/base) * pow(base, power + 1)
+  }
+
 }
 
 module.exports = pow;
