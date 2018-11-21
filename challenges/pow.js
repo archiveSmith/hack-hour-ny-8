bf3b030fc1ee67c9644a98329dd2819787303b3a
01212 <marlonwiprud1@gmail.com>
2018-11-21 09:57:39 -0500

    handled negative power edge case

--- challenges/pow.js
+++ challenges/pow.js
@@ -3,14 +3,22 @@
  */
 
 function pow(base, power, beencalled, multiplier) {
+
+  if (beencalled === undefined && power < 0) {
+    return 1 / pow(base * base, Math.abs(power) - 1, true, base)
+  }
   if (beencalled === undefined) {
     return pow(base * base, power - 1, true, base)
   }
   if (power === 1) return base;
 
+
+
   return pow(base * multiplier, power - 1, true, multiplier)
 }
 
-// console.log(pow(5, 2))
+console.log(pow(5, -2))
+
+
 
 module.exports = pow;
