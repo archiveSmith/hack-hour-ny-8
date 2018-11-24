c2b02469d8153e2af8278607881b1961bff38e11
Taihyun Lee <th9061@hotmail.com>
2018-11-24 09:29:53 -0500

    completed string rotation

--- challenges/string-rotation.js
+++ challenges/string-rotation.js
@@ -16,7 +16,14 @@ function isSubstring(s1, s2) {
 }
 
 function stringRotation(s1, s2) {
+  if(s1.length !== s2.length) {
+    return false;
+  }
 
+  let result = s1 + s1;
+  return isSubstring(result, s2);
 }
+
+// console.log(stringRotation("hello", "ollhe"));
 
 module.exports = {isSubstring: isSubstring, stringRotation: stringRotation};
